6797a9b017ae5353f13042db84bc2c47fa5e8a0f
ArturoYanez <Arturo_w_0@hotmail.com>
2023-12-03 04:55:54 +0100

    Fase 3 culminada, detalles por fixear en algunas plantillas
    Sistema de cliente realizado con vistas de productos y detalles

--- index.js
+++ index.js
@@ -1,55 +1,37 @@
-//Librerias y dependencias
 require('dotenv').config();
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
 const app = express();
-const path = require('path');
 const bodyParser= require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-const baseDatosModels = require('./models/database.js');
-const {PASSWORD,ADMIN} = process.env;
-let login= false;
+//app.use(bodyParser.urlencoded({extended: true}));
+const path = require('path');
+const baseDatos = require('./models/database.js');
+const utils = require('./utils/uploadImg.js');
+const {administrador,password} = process.env;
 let ext;
+app.use(express.json());
+let login= false;
 
-//Randis Graterol
 
-function getContentType(extname) {
-  switch (extname) {
-    case '.html':
-      return '.html';
-    case '.css':
-      return '.css';
-    case '.js':
-      return '.js';
-    case '.json':
-      return '.json';
-    case '.png':
-      return '.png';
-    case '.jpg':
-      return '.jpg';
-    case 'jpeg':
-      return '.jpeg';
-    default:
-      return null;
-  }
-}
-
+//--------------------------------------------------------------
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './static/uploads')
   },
   filename: function (req, file, cb) {
     ext = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + Date.now() + getContentType(ext))
+    cb(null, file.fieldname + '-' + Date.now() + utils.getContentType(ext))
   }
 })
 
 let upload = multer({ storage: storage });
+//---------------------------------------------------------------
+
 //recursos que se van a cargar en el server 
 app.use(express.static(__dirname+'/static'));
 
-
+//-----------------------------------------------------------------
 //Configuración del Servidor
 app.set('view engine','ejs');//definimos el motor de plantilla con archivos ejs
 app.set('views',path.join(__dirname,"./views"));//definimos la ruta del motor de plantilla
@@ -59,14 +41,14 @@ console.log('Servidor corriendo exitosamente en el puerto 5000');
 
 
 
-
+//-----------------------------------------------------------
 //enruptamiento
 app.get('/',(req,res)=>{
   res.render('index.ejs')
 });
 
 app.get('/login',(req,res)=>{
-res.render('iniciarSesion.ejs');
+  res.render('iniciarSesion.ejs');
 });
 
 
@@ -74,85 +56,127 @@ app.post('/login',(req,res)=>{
 
  const {admin,password} = req.body;
 
-   if(admin === ADMIN && password === PASSWORD){
-    login=true;
-    res.redirect('/productos');
-   }else{
-    login=false;
-    res.redirect('/iniciarSesion');
-   }
+ if(admin === administrador && password === password){
+  login=true;
+  res.redirect('/productos');
+}else{
+  login=false;
+  res.redirect('/*');
+}
 
 });
-  
-/* ADD */
+
+
 app.get('/add',(req,res)=>{
-res.render('add.ejs');
+  res.render('add.ejs');
 });
 
-app.get('/addImagen',(req,res)=>{
-res.render('addImagen.ejs');
+//---------------------------------------------------------
+app.get('/addImagen/:id',(req,res)=>{
+  baseDatos.getImagen(req,res);
 });
 
-app.post('/addImagen',upload.single('img'),(req,res)=>{
-baseDatosModels.aggIMG(req,res);
+
+app.post('/addImagen/:id',upload.single('img'),(req,res)=>{ 
+  baseDatos.aggIMG(req,res);
 });
+
 
 app.post('/addPost',(req,res)=>{   
-baseDatosModels.aggDato(req,res);
+  baseDatos.aggDato(req,res);
 });
 
-// PRODUCTOS
+
 app.get('/productos',(req,res)=>{
-  baseDatosModels.mostrarProductos(req,res);
+  baseDatos.mostrarProductos(req,res);
 });
-
-
+//-------------------------------------------------------
 // GET /editar/:id
 app.get('/update/:id',(req, res) => {
-baseDatosModels.mostrarUpdate(req,res);
+  baseDatos.mostrarUpdate(req,res);
 
 });
+//-------------------------------------------------------
 // POST /editar/:id
 app.post('/update/:id', (req, res) => {
- baseDatosModels.update(req,res);
+ baseDatos.update(req,res);
 });
-
 //-------------------------------------------------------
 // GET /eliminar/:id
 app.get('/delete/:id', (req, res) => {
- baseDatosModels.mostrarDelete(req,res);
+ baseDatos.mostrarDelete(req,res);
 });
 //-------------------------------------------------------
 // POST /eliminar/:id
 app.post('/delete/:id', (req, res) => {
- baseDatosModels.deletee(req,res);
+ baseDatos.deletee(req,res);
 });
-
-
-//-------------------------------------------------------
-// Categorias
+//------------------------------------------------------
 app.get('/categorias', (req, res) => {
- baseDatosModels.getCategorias(req,res);
+ baseDatos.getCategorias(req,res);
 });
-
-// Add -> Categorias
+//-------------------------------------------------------
 app.get('/addCategorias', (req, res) => {
  res.render('addcategoria.ejs');
 });
+//-------------------------------------------------------
 app.post('/addcategorias', (req, res) => {
- baseDatosModels.postCategorias(req,res);
+ baseDatos.postCategorias(req,res);
 });
-// Update -> Categorias
+//-------------------------------------------------------
 app.get('/updateCategoria/:id',(req,res)=>{
- baseDatosModels.mostrarUpdateC(req,res);
+ baseDatos.mostrarUpdateC(req,res);
 });
+//-------------------------------------------------------
 app.post('/updateCategoria/:id',(req,res)=>{
-baseDatosModels.updateCateg(req,res);
+  baseDatos.updateCateg(req,res);
 });
+//-------------------------------------------------------
+app.get('/eliminarCategoria/:id',(req,res)=>{
+  baseDatos.deleteCategoriaGET(req,res);
+})
+//-------------------------------------------------------
+app.get('/clientes',(req,res)=>{
+  console.log('mostrando pagina la cliente!');
+  baseDatos.ClientesGET(req,res);
+})
+//-------------------------------------------------------
+app.post('/cliente', (req, res) => {
+ baseDatos.filtrar(req,res);
+});
+//-------------------------------------------------------
+app.get('/clientico', (req, res) => {
+ baseDatos.filtrar2(req,res);
+});
+//-------------------------------------------------------
+app.get('/detalles/:id',(req,res)=>{
+  baseDatos.detalles(req,res);
+});
+//-------------------------------------------------------
+app.get('/ruta', (req, res) => {
+  const {nombre,codigo,precio,descripcion,calidad,cantidad,url} = req.query;
 
+  let datos = {
+    nombre:nombre,
+    codigo:codigo,
+    precio:precio,
+    descripcion:descripcion,
+    calidad:calidad,
+    cantidad:cantidad,
+    url:url
+  }
 
+  console.log(datos,'Valor de Busqueda--por fin');
+  res.render('buscar.ejs',{result:datos});
+
+});
+//-------------------------------------------------------
+app.get('/detalles/:id',(req,res)=>{
+  baseDatos.detalles(req,res);
+});
+//-------------------------------------------------------
 //Metodo para manejar rutas no encontradas
 app.get('/*',(req,res)=>{
-res.render('notfound.ejs')
+  res.render('notfound.ejs');
 });
-
+//-------------------------------------------------------
--- models/database.js
+++ models/database.js
@@ -1,68 +1,71 @@
+// -*- coding: utf-8 -*-
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
 
 // Crear la base de datos
 const dbname = path.join(__dirname,'../db','base.db');
-const db = new sqlite3.Database(dbname,err=>{
+const db = new sqlite3.Database(dbname,{verbose:true,charset:'utf8'},err=>{
   if(err) return console.error(err.message);
   console.log('Conexion Exitosa con la Base de Datos')
 });
 
 db.serialize(() => {
 // Crear la tabla "categorias" si no existe
-db.run(`
-  CREATE TABLE IF NOT EXISTS categorias (
+  db.run(`
+    CREATE TABLE IF NOT EXISTS categorias (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL
-  )
-`);
+    )
+    `);
 //------------------------------------------------------------------
 // Crear la tabla "Productos" si no existe
-db.run(`
-  CREATE TABLE IF NOT EXISTS productos (
+  db.run(`
+    CREATE TABLE IF NOT EXISTS productos (
     producto_id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
     codigo INTEGER NOT NULL,
     precio INTEGER NOT NULL,
     descripcion TEXT NOT NULL,
     calidad TEXT NOT NULL,
-    cantidad TEXT NOT NULL,
+    cantidad INTEGER NOT NULL,
     categoria_id INTEGER,
     FOREIGN KEY (categoria_id) REFERENCES categorias(id)
-  )
-`);
+    )
+    `);
 //-----------------------------------------------------------------
-db.run(`
-  CREATE TABLE IF NOT EXISTS imagenes (
+  db.run(`
+    CREATE TABLE IF NOT EXISTS imagenes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     url TEXT NOT NULL,
-    destacado TEXT
-  )
-`);
+    destacado INTEGER DEFAULT 0,
+    productoID INTEGER,
+    FOREIGN KEY (productoID) REFERENCES productos(producto_id)
+    )
+    `);
 
 });
 
 function aggDato(req,res){
-const {nombre,codigo,precio,descripcion,calidad,cantidad,categoria} = req.body;
-    
-const sql = `INSERT INTO productos(nombre, codigo, precio, descripcion, calidad, cantidad,categoria_id) 
-    VALUES (?, ?, ?, ?, ?, ? , ?)`;
+  const {nombre,codigo,precio,descripcion,calidad,cantidad,categoria} = req.body;
 
-db.run(sql, [nombre, codigo, precio, descripcion, calidad, cantidad,categoria], err => {
+  const sql = `INSERT INTO productos(nombre, codigo, precio, descripcion, calidad, cantidad , categoria_id) 
+  VALUES (?, ?, ?, ?, ?, ? , ?)`;
+
+  db.run(sql, [nombre, codigo, precio, descripcion, calidad, cantidad , categoria], err => {
     if (err) return console.error(err.message);
     console.log('Registros Ingresados Correctamente a la base de datos ');
     res.redirect('/productos');
-});
+  });
 }
 
 function mostrarProductos(req,res){
  const sql = `SELECT * FROM productos`;
  db.all(sql,[],(err,rows)=>{
- console.log(rows,);
- if(err) return console.error(err.message);
- console.log('Leyendo Tabla productos...')
- res.render('productos.ejs',{modelo:rows});
+  console.log(rows,);
+  if(err) return console.error(err.message);
+  console.log('Leyendo Tabla productos...')
+  res.render('productos.ejs',{modelo:rows});
 })
 }
 
@@ -82,19 +85,34 @@ function update(req,res){
 
   const {nombre,codigo,precio,descripcion,calidad,cantidad,categoria} = req.body;
   
-  const sql = `UPDATE productos SET nombre = ?, codigo = ?, precio = ?, descripcion = ?, calidad = ?, cantidad = ? , categoria_id = ? WHERE producto_id = ?`;
+  const sql = `UPDATE productos SET nombre = ?, codigo = ?, precio = ?, descripcion = ?, calidad = ?, cantidad = ?, categoria_id = ? WHERE producto_id = ?`;
 
-  db.run(sql, [nombre,codigo, precio,descripcion,calidad, cantidad,categoria,id], err => {
-  if (err) return console.error(err.message);
-  console.log(`producto actualizado = Producto : ${id}`);
-  res.redirect('/productos');
+  db.run(sql, [nombre,codigo, precio,descripcion,calidad, cantidad , categoria , id], err => {
+    if (err) return console.error(err.message);
+    console.log(`producto actualizado = Producto : ${id}`);
+    res.redirect('/productos');
   });
 
 }
-
+//-----------------------------------------------
+function getImagen(req,res){
+  const id = req.params.id; 
+  const sql = `SELECT * FROM productos WHERE producto_id = ?`
+  db.get(sql, [id], (err, row) => {
+    console.log(row,'+++++++++++++++');
+    if (err){
+      res.status(500).send({ error: err.message });
+      return console.error(err.message);
+    }
+    res.render('addImagen.ejs', { modelo:row});
+  });
+ //const sql = `SELECT * FROM productos WHERE producto_id = ?`; 
+}
+//---------------------------------------------------------
 function mostrarDelete(req,res){
-    const id = req.params.id;
+  const id = req.params.id;
   const sql = `SELECT * FROM productos WHERE producto_id = ?`;
+
   db.get(sql, [id], (err, row) => {
     if (err){
       res.status(500).send({ error: err.message });
@@ -106,35 +124,36 @@ function mostrarDelete(req,res){
 
 function deletee(req,res){
  const id = req.params.id;
-  console.log('Consulta Eliminar');
-  const sql = `
-    DELETE FROM productos WHERE producto_id = ?
-  `;
-  db.run(sql, [id], err => {
-    if (err) {
-      res.status(500).send({ error: err.message });
-      return console.error(err.message);
-    }
-    console.log('Producto eliminado');
-    res.redirect('/productos');
-  });
+ console.log('Consulta Eliminar');
+ const sql = `
+ DELETE FROM productos WHERE producto_id = ?
+ `;
+ db.run(sql, [id], err => {
+  if (err) {
+    res.status(500).send({ error: err.message });
+    return console.error(err.message);
+  }
+  console.log('Producto eliminado');
+  res.redirect('/productos');
+});
 }
 
 //_-------------------------------------------------
 function aggIMG(req,res){
+  const id = req.params.id;
   console.log(req.file);
-let ruta = req.file.path.split('\\');
-const file = `/${ruta[1]}/${ruta[2]}`;
-console.log(file);
-const {destacado} = req.body;
-const sql = `INSERT INTO imagenes(url,destacado) 
-    VALUES (?,?)`;
+  let ruta = req.file.path.split('\\');
+  const file = `/${ruta[1]}/${ruta[2]}`;
+  console.log(file);
+  const {destacado} = req.body;
+  const sql = `INSERT INTO imagenes(url,destacado,productoID) 
+  VALUES (?,?,?)`;
 
-db.run(sql, [file,destacado], err => {
+  db.run(sql, [file,destacado,id], err => {
     if (err) return console.error(err.message);
     console.log('URL de imagen Insertada Correctamente');
     res.redirect('/productos');
-});
+  });
 
 }
 //----------------------------------------------------
@@ -142,31 +161,31 @@ db.run(sql, [file,destacado], err => {
 function getCategorias(req,res){
  const sql = `SELECT * FROM categorias`;
  db.all(sql,[],(err,rows)=>{
- console.log(rows,);
- if(err) return console.error(err.message);
- console.log('Leyendo Tabla categorias...')
- res.render('categorias.ejs',{modelo:rows});
-})
+   console.log(rows,);
+   if(err) return console.error(err.message);
+   console.log('Leyendo Tabla categorias...')
+   res.render('categorias.ejs',{modelo:rows});
+ })
 }
 
 function postCategorias(req,res){
-const {nombre} = req.body
-const sql = `INSERT INTO categorias(nombre) 
-    VALUES (?)`;
+  const {nombre} = req.body
+  const sql = `INSERT INTO categorias(nombre) 
+  VALUES (?)`;
 
-db.run(sql, [nombre], err => {
+  db.run(sql, [nombre], err => {
     if (err) return console.error(err.message);
     console.log('categoria ingresada correctamente');
     res.redirect('/categorias');
-});
+  });
 
 }
 
 function mostrarUpdateC(req,res){
   //Este metodo es el GET
-    const id = req.params.id;
-    const sql = `SELECT * FROM categorias WHERE id = ?`;
-    db.get(sql, [id], (err, row) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM categorias WHERE id = ?`;
+  db.get(sql, [id], (err, row) => {
     console.log(row);
     if (err) return console.error(err.message);
     res.render('updateCategorias.ejs', {modelo:row});
@@ -188,6 +207,116 @@ function updateCateg(req,res){
   });
 
 }
+//------------------------------------------------------------
+function filtrar(req,res){
+
+const busqueda = req.body.busqueda;
+console.log(busqueda,"dato filtrar----");
+
+const sql = `SELECT productos.*, imagenes.url
+FROM productos
+LEFT JOIN imagenes ON productos.producto_id = imagenes.productoID WHERE nombre = ? OR descripcion = ? OR categoria_id = ? OR calidad = ? OR cantidad = ?`;
+
+db.all(sql,[busqueda,busqueda,busqueda,busqueda,busqueda],(err,rows)=>{
+  console.log(rows,'base de datos desde la peticion del cliente');
+
+  if(err){
+     console.error(err.message);
+      res.status(500).send('Error en el servidor');
+      return;
+  }
+
+ res.json({producto:rows});
+})  
+  
+}
+//-------------------------------------------------------
+function filtrar2(req,res){
+
+const busqueda = req.query.busqueda;
+
+const sql = `SELECT productos.*, imagenes.url
+FROM productos
+LEFT JOIN imagenes ON productos.producto_id = imagenes.productoID WHERE nombre = ? OR descripcion = ? OR categoria_id = ? OR calidad = ? OR cantidad = ?`;
+
+db.all(sql,[busqueda,busqueda,busqueda,busqueda,busqueda],(err,rows)=>{
+  console.log(rows,'base de datos');
+
+  if(err){
+     console.error(err.message);
+      res.status(500).send('Error en el servidor');
+      return;
+  }
+
+ res.json({producto:rows})
+
+})  
+  
+}
+//-------------------------------------------------------
+  function ClientesGET(req,res){
+  
+  const sql =`SELECT p.*, i.url AS imagen_url
+             FROM productos p
+             LEFT JOIN imagenes i ON p.producto_id = i.productoID
+             WHERE i.destacado = 1
+             GROUP BY p.producto_id`;
+
+  db.all(sql,[],(err, rowsProduct) => {
+    console.log(rowsProduct,'....+..+....');
+  
+//-------------------------------------------
+       if (err){
+      console.error(err.message);
+      res.status(500).send('Error en el servidor');
+      return;
+    } 
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      // Envío de la respuesta con los resultados
+      res.render('clientes.ejs',{
+        producto:rowsProduct
+      });
+      
+
+
+    });
+
+}
+//--------------------------------------------------
+function detalles(req,res){
+  const id = req.params.id;
+  const sql = `SELECT imagenes.url, productos.*
+               FROM imagenes
+               INNER JOIN productos ON imagenes.productoID = productos.producto_id
+               WHERE imagenes.productoID = ?`;
+  db.all(sql,[id],(err,rowsImagenes)=>{
+    console.log(rowsImagenes,'plantilla detalles');
+     if (err){
+      console.error(err.message);
+      res.status(500).send('Error en el servidor');
+      return;
+    } 
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      // Envío de la respuesta con los resultados
+      res.render('detalles.ejs',{
+        imagenes:rowsImagenes
+      });
+
+  });
+}
+function deleteCategoriaGET(req,res){
+const id = req.params.id;
+const sql = ` DELETE FROM categorias WHERE id = ?`;
+ db.run(sql, [id], err => {
+    if (err) {
+      res.status(500).send({ error: err.message });
+      return console.error(err.message);
+    }
+    console.log('Categoria eliminada');
+    res.redirect('/categorias');
+  });
+}
+//--------------------------------------------------
 
 //_-------------------------------------------------
 module.exports={
@@ -201,6 +330,11 @@ module.exports={
  getCategorias,
  postCategorias,
  mostrarUpdateC,
- updateCateg
- 
+ updateCateg,
+ filtrar,
+ filtrar2,
+ ClientesGET,
+ getImagen,
+ detalles,
+ deleteCategoriaGET
 }
